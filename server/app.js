55f99a4dfdb91c5815b745c400e53909ddcc665a
Nina Wang <115374853+ninaw04@users.noreply.github.com>
2024-04-01 23:18:12 -0700

    erase connection info

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: 'db-mysql-web-store-do-user-16094181-0.c.db.ondigitalocean.com',
-  user: 'business',
-  port: 25060,
-  password: 'MuscleM0mmy',
-  database: 'webstore'
+  host: '',
+  user: '',
+  port: ,
+  password: '',
+  database: ''
 })
 
 connection.connect(function(err)  {
