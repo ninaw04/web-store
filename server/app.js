5db130121decfca6b773e2dfc867aed0b39063af
ninaw04 <ninawangcn@gmail.com>
2024-04-18 14:44:23 -0700

    :fire: rerouting and api call skeleton

--- server/app.js
+++ server/app.js
@@ -1,51 +1,19 @@
-var createError = require("http-errors");
-var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+
+const express = require("express")
+const app = express();
+const cors = require("cors");
+const PORT = 8800;
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var usersRouter = require("./routes/buyers");
 
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-const cors = require("cors");
 app.use(express.static(path.join(__dirname, "public")));
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "",
-  user: "",
-  port: ,
-  password: "",
-  database: "",
-});
 
+app.use(express.json());
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/buyers", usersRouter);
 app.use(cors());
-// console.log("here");
-connection.connect(function (err) {
-  console.log("Might be errro");
-  if (err) throw err;
-  console.log("You are now connneecetedfse");
-});
-// catch 404 and forward to error handler
-app.get("/products", (req, res) => {
-  connection.query("SELECT * FROM products", function (err, result, fields) {
-    if (err) {
-      console.error("Could not fetch data from products table");
-    }
-    res.json(result);
-  });
-});
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -61,4 +29,9 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+app.listen(PORT, () => {
+  console.log(`connected on http://localhost:${PORT}`)
+})
+
 module.exports = app;
