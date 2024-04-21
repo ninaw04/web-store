77e296304f2c9095001316bed622253a6d02f9bd
vihashah <viha.shah@sjsu.edu>
2024-04-20 19:20:24 -0700

    :see_no_evil:

--- .userConfig.js
+++
@@ -1,7 +0,0 @@
-exports.connect = {
-  host: "db-mysql-web-store-do-user-16094181-0.c.db.ondigitalocean.com",
-  user: "business",
-  port: 25060,
-  password: "MuscleM0mmy",
-  database: "webstore"
-}
