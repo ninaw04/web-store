415b6d98f9cfffb8aec9af75f77364c3f0379b3b
ninaw04 <ninawangcn@gmail.com>
2024-04-23 22:57:34 -0700

    :card_file_box: buyers crud calls

--- server/routes/buyers.js
+++ server/routes/buyers.js
@@ -8,14 +8,43 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* GET specific buyer */
-router.get('/', (req,res) => {
+/* Make a new user */
+router.post('/', (req, res) => {
+  const q = 'INSERT INTO Buyer (`fName`, `lName`) VALUES (?)'
+  const values = [
+    req.body.fName,
+    req.body.lName
+  ]
 
+  pool.query(q, [values], (err, data) => {
+    if(err) return res.json(err)
+    return res.json("User has been created")
+})
+})
+
+/* GET specific buyer */
+router.get('/:id', (req,res) => {
+  const {id} = req.params
+  const q = `SELECT * FROM Buyer where buyerID = ${id}`
+
+  pool.query(q, (err, data) => {
+      if(err) return res.json(err)
+      return res.json(data)
+  })
 })
 
 /* GET cart */
 router.get('/:id/cart', (req, res) => {
+  // select cart and products from cart
+  const {id} = req.params
+  const q = `SELECT * FROM Cart LEFT JOIN Product
+            ON Cart.productID = Product.productID
+            WHERE buyerID = ${id}`
 
+  pool.query(q, (err, data) => {
+    if(err) return res.json(err)
+    return res.json(data)
+  })
 })
 
 module.exports = router;
