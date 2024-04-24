6f7cb7c57c6adbe928f074fbb1da7cb0fd22867a
vihashah <viha.shah@sjsu.edu>
2024-04-23 23:29:55 -0700

    :card_file_box: updated backend to include basic product crud

--- server/routes/index.js
+++ server/routes/index.js
@@ -22,7 +22,45 @@ router.get("/products", (req, res) => {
     res.json(result);
   });
 });
+router.get("/products/product/:productId", (req, res) => {
+  pool.query(`SELECT * FROM products WHERE productId = ${req.params.productId}`, function(err, result, fields) {
+    if (err) {
+      console.error("SQL ERROR products/:productId");
+      return 
+    }
+    res.json(result);
+  })
+});
 
+router.get("/products/search/:searchItem", (req, res) => {
+  console.log(req.query);
+
+  pool.query(`SELECT * FROM products WHERE productName LIKE '${req.params.searchItem}%'`, function(err, result, fields) {
+    if (err) {
+      console.error("SQL ERROR products/search/:searchItem");
+      return 
+    }
+    console.log(result)
+    res.json(result);
+  })
+});
+//ASSUME THAT MINCOST AND MAXCOST ARE PROVIDED. By default they should be between 0 and the 1000 or something
+router.get("/products/filter", (req, res) => {
+  console.log(req.body);
+  if (req.body.minCost && req.body.maxCost){ //if both minCost and maxCost are provided
+    pool.query(`SELECT * FROM products WHERE price BETWEEN ${req.body.minCost} and ${req.body.maxCost}`, function(err, result, fields) {
+      if (err) {
+        console.error("SQL ERROR");
+        return 
+      }
+      console.log(result)
+      res.json(result);
+    })
+  } 
+  // console.log(req.query);
+
+  
+});
 
 router.get("/checkout", (req, res) => {
   
