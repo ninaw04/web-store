581c0300f5f0dca3e4965773043917ca6e10be5c
ninaw04 <ninawangcn@gmail.com>
2024-04-23 21:50:56 -0700

    :fire: housekeeping

--- server/routes/buyers.js
+++ server/routes/buyers.js
@@ -3,17 +3,6 @@ var router = express.Router();
 
 let pool = require('../database').pool
 
-/* GET users listing. */
-// pool.getConnection((err, connection) => {
-//   if (err) throw err;
-
-//   connection.query('/', (err, result) => {
-//     if(err) throw err;
-//     res.send('respond with a resource');
-
-//     connection.release();
-//   });
-// })
 router.get('/', function(req, res, next) {
   console.log("HEREERERERERERE")
   res.send('respond with a resource');
--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom"
 
 import CheckoutPage from "./Components/Checkout";
-import ProductPage from "./Components/ProductPage";
 import Box from '@mui/material/Box'
-import Carousel from './Components/Carousel';
 import HomePage from './Components/HomePage'
 function App() {
   
